0528dac40560531c1ce3f9237f2dd8adcac8691f
Artem280590 <gonya90.ag@gmail.com>
2023-02-22 20:44:52 +0200

    Got API KEY Created models Created an interface with requests Set up Retrofit

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "38b1aeb674ff4c3bb3b77a19cdeff49f"
+    }
+}
