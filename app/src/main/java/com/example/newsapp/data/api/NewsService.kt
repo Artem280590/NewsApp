699a18d41901658f99d665e044ee6dcdc6ed6766
Artem280590 <gonya90.ag@gmail.com>
2023-03-16 15:56:10 +0200

    Created a DAO for an article Created a local database DI HILT setting

--- app/src/main/java/com/example/newsapp/data/api/NewsService.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsService.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.data.api
 
+import com.example.newsapp.models.NewsResponse
 import com.example.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,7 +14,7 @@ interface NewsService {
         @Query("q") query: String,
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ) : Response<NewsResponse>
 
 
     @GET("/v2/top-headlines")
@@ -20,6 +22,6 @@ interface NewsService {
         @Query("country") countryCode: String = "ua",
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    )
+    ) : Response<NewsResponse>
 
 }
