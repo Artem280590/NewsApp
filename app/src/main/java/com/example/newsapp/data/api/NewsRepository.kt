4c44d43eb13b3889ff790a098d6083a790419e03
Artem280590 <gonya90.ag@gmail.com>
2023-03-20 20:50:01 +0200

    Made Search fragment Created and completed RecyclerView Got a list of news by key and passed them to the RecyclerView

--- app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.example.newsapp.data.api
 
+import retrofit2.http.Query
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(private val newsService: NewsService){
     suspend fun getNews(contryCode: String, pageNumber: Int) =
         newsService.getHeadlines(countryCode = contryCode, page = pageNumber)
+
+    suspend fun getSearchNews(query: String, pageNumber: Int) =
+        newsService.getEverything(query = query, page = pageNumber)
 }
